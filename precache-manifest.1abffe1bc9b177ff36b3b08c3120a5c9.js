f42966b419e8d27fea4d7ae97d7eb140d8f63fd6
DaniNg98 <36421880+DaniNg98@users.noreply.github.com>
2021-01-29 12:29:47 +0100

    Updates

---
+++ precache-manifest.1abffe1bc9b177ff36b3b08c3120a5c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd880dba93df18651c8702197f6d4215",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "0e552d0b2549bfbbb176",
+    "url": "/react-deploy/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "631e961ecc420674d2cf",
+    "url": "/react-deploy/static/js/2.d9093ec4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.d9093ec4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e552d0b2549bfbbb176",
+    "url": "/react-deploy/static/js/main.cc7265f5.chunk.js"
+  },
+  {
+    "revision": "610108ecd93b7ae51db8",
+    "url": "/react-deploy/static/js/runtime-main.3b9bd870.js"
+  }
+]);
